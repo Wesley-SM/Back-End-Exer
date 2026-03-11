924e4738f32d3fc38b3f8bce5d8b8de29983ea30
Wesley-SM <oficial.wesleygaum@gmail.com>
2026-03-11 12:10:33 -0300

    priemiro update exercicio 1

---
+++ src/Exercicio01/exe2.java
@@ -0,0 +1,16 @@
+package Exercicio01;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+
+
+public class exe2 {
+
+    static void main(){
+
+
+        String trabalho= "Aprendiz Backend";
+
+        System.out.println("nome: Wesley Machado");
+        System.out.println("Cargo:" + trabalho);
+        System.out.println("Salário: R$ 500.00");
+
+    }
+}
